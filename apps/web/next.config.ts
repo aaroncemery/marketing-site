4b42589e8dd5560ad088516bb725ded0df69d39e
Aaron Emery <aaron.emery@sanity.io>
2025-05-04 08:52:46 -0700

    feat: use-is-mobile hook

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+    inlineCss: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
