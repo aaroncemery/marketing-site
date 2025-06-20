390d267d0d6380df3195c794aa25b81b97d7774b
Aaron Emery <aaron.emery@sanity.io>
2025-06-20 11:23:09 -0700

    feat: adding test-function

--- apps/sanity-functions/sanity.blueprint.ts
+++ apps/sanity-functions/sanity.blueprint.ts
@@ -2,6 +2,13 @@ import { defineBlueprint, defineDocumentFunction } from "@sanity/blueprints";
 
 export default defineBlueprint({
   resources: [
-    // defineDocumentFunction({name: 'my-function'}),
+    defineDocumentFunction({
+      name: "test-function",
+      event: {
+        on: ["publish"],
+        filter: '_type == "post"',
+        projection: "_id, content",
+      },
+    }),
   ],
 });
